d997292c5c9cfe4e6fabb0261d3eb47c785fb83e
Ashiqul Islam Ayon <ashiqulislamayon28@gmail.com>
2024-12-16 23:39:41 +0600

    basic setup done

---
+++ frontend/chess/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Chessboard } from "react-chessboard";
+import PlayRandomMoveEngine from './components/ChessBoard';
+
+function App() {
+
+  return (
+    <>
+      <PlayRandomMoveEngine />
+    </>
+  )
+}
+
+export default App
